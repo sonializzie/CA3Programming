bcd74db34e992300379029cbd103048659633cbb
Sonia Ndonga <ndongasonia@gmail.com>
2024-03-31 21:49:36 +0100

    Create ReportGenerator.java
    - Created a ReportGenerator interface

---
+++ src/ca3programming/reports/ReportGenerator.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package ca3programming.reports;
+
+import java.util.List;
+
+/**
+ *
+ * @author sonia_ndonga
+ */
+public interface ReportGenerator {
+    // Beginning of ReportGenerator Interface
+
+    // List Array for generateReport
+    List<String> generateReport();
+
+}// End of ReportGenerator Interface
